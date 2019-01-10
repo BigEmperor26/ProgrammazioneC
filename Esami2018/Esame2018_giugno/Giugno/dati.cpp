416ce3e12a6948fafce161a5992d63338106fb63
BigEmperor26 <mickyin3@gmail.com>
2019-01-10 11:16:31 +0100

    Update dati.cpp

--- Esami2018/Esame2018_giugno/Giugno/dati.cpp
+++ Esami2018/Esame2018_giugno/Giugno/dati.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void creaSpedizione(Tspedizione* s){
     s->tipoPacco= Tpacco (rand()%3);
     do{
+            printf("peso: ");
             scanf("%f",&(s->peso));
     }while(s->peso < 200.00 || s->peso > 5000.00 );
     if(s->tipoPacco > 0){
@@ -14,13 +15,25 @@ void creaSpedizione(Tspedizione* s){
     }else{
         s->assicurato=false;
     }
+    printf("destinatario: ");
     scanf("%s",s->destinatario);
 }
 
 void addPacco(TcodaFIFO* p[],Tspedizione s){
     int n=rand()%3;
-    TcodaFIFO* tmp= new TcodaFIFO(p[n],s);
-    p[n]=tmp;
+    //TcodaFIFO* tmp= new TcodaFIFO(p[n],s); implementazione LIFO
+    //p[n]=tmp;
+    //implementazione FIFO
+    TcodaFIFO* tmp=p[n];
+    if(tmp==NULL){
+        TcodaFIFO* tmp= new TcodaFIFO(NULL,s);
+        p[n]=tmp;
+    }else{
+        while(tmp->next!=NULL){
+            tmp = tmp->next;
+        }
+        tmp->next = new TcodaFIFO(NULL,s);
+    }
 }
 void stampaSpedizioni_(TcodaFIFO *p){
     TcodaFIFO* tmp=p;
@@ -44,7 +57,7 @@ TcodaFIFO* delPacchi(TcodaFIFO* p[],Tpacco tp,int n){
 }
 
 void delPacchi_(TcodaFIFO** p,Tpacco tp){
-    TcodaFIFO* tmp=*p;;
+    TcodaFIFO* tmp=*p;
     while(tmp!=NULL){
         if(tmp->dato.tipoPacco == tp){
             FILE* fp=fopen("pacchiOrdinari","w");
